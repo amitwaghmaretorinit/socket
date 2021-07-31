1fd47b369e691503221b0b1ee3043cdb2ab0cfbe
amit <amit@torinit.ca>
2021-08-01 00:31:31 +0530

    node modules removed

--- src/components/UserItem/index.js
+++ src/components/UserItem/index.js
@@ -4,13 +4,11 @@ function UserItem({ name, id, latestMessage, avatar }) {
   return (
     <div className={"user_item"}>
       <img src={avatar} alt="user" className={"avatar"} />
-      <div
-        className={'user_msg'}
-      >
-        <span className={'name'}>
+      <div className={"user_msg"}>
+        <span className={"name"}>
           {name} {id}
         </span>
-        <span className={'latest_message'}>{latestMessage}</span>
+        <span className={"latest_message"}>{latestMessage}</span>
       </div>
     </div>
   );
