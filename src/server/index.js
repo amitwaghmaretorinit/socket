1bc5ae31bb752f6b330b501096e716286cf7b56f
amit <amit@torinit.ca>
2021-08-04 00:08:32 +0530

    minor changes

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,33 @@
 import express from "express";
- 
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
-const PORT = process.env.PORT || 5000;
 const app = express();
-app.use(express.json());
+app.use(cors());
+app.options("*", cors());
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("join", ({ name, room }) => {
+    console.log({ name, room });
   });
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
+server.listen(4000, () => {
+  console.log("server started at", 4000);
+});
